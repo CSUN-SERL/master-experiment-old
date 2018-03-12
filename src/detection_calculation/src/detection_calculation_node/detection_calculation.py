7cbc85fab7ed1fc1f485df9f103f7f43a9d51026
BasedLemur <logangerber1014@yahoo.com>
2018-03-11 21:03:15 -0700

    Now accurately gets a list of all humans within the field of view, as well as location information.

--- src/detection_calculation/src/detection_calculation_node/detection_calculation.py
+++ src/detection_calculation/src/detection_calculation_node/detection_calculation.py
@@ -21,13 +21,10 @@ global init_robot_pose
 
 image_arr = []
 
-human_msg_ = Human()
-compiled_msgs_ = CompiledFakeMessage()
-
 
 #Config file dictinary
-MyHumans = yaml.load(open('/home/serl/sarwai-experiment/human.yaml'))
-init_robot_pose = yaml.load(open('/home/serl/sarwai-experiment/robot.yaml'))
+MyHumans = yaml.load(open('/home/serl/sarwai-experiment-fd/human.yaml'))
+init_robot_pose = yaml.load(open('/home/serl/sarwai-experiment-fd/robot.yaml'))
 
 global max_distance
 
@@ -54,17 +51,15 @@ def process():
 	max_distance = init_robot_pose[mission_number_][str(robot_number_)]['dof']
 	robot_fov = init_robot_pose[mission_number_][str(robot_number_)]['fov']
 
+	global FOV_MARGIN
+	FOV_MARGIN = 0.087 / 2.0
+
 	global pub
 	pub = rospy.Publisher('sarwai_detection/custom_msgs_info', CompiledFakeMessage, queue_size=1000)
-	
-	global imgpub
-	imgpub = rospy.Publisher('test_img_topic', Image, queue_size = 100)
+
 	rospy.Subscriber('robot' + str(robot_number_) + '/odom', Odometry, Odometry_update, queue_size=1)
 	
 	rospy.Subscriber('coffee', String, Force_update, queue_size = 1)
-
-	global counter
-	counter = 0
 
 	rospy.spin()
 
@@ -72,10 +67,9 @@ def Force_update(data):
 	print("Got force")
 
 def Odometry_update(data):
-	# print('******************************ODOM UPDAAAAATTTEEEEE')
 	#Getting x and z change for robot
-	x = data.pose.pose.position.x
-	y = data.pose.pose.position.y
+	x = data.pose.pose.position.x * -1
+	y = data.pose.pose.position.y * -1
 
 	#Getting the Quaternion info
 	xQ = data.pose.pose.orientation.x
@@ -86,58 +80,28 @@ def Odometry_update(data):
 	# Converting quaternion to euler angle
 	xE,yE,zE = quaternion_to_euler_angle(xQ,yQ,zQ,wQ)
 
+
 	#Robot position constantly updated
-	#global robot_pos_x, robot_pos_y, robot_pos_th
 	new_x_pos = robot_pos_x + x
 	new_y_pos = robot_pos_y + y
 	new_th_pos = robot_pos_th + zE
 
 	#Searching for humans
-	find(new_x_pos, new_y_pos, new_th_pos)
+	human_list = find(new_x_pos, new_y_pos, new_th_pos)
 
 	#Msgs being set and released
-	compiled_msgs_.header.stamp = rospy.Time.now()
+	new_message = CompiledFakeMessage()
+	new_message.humans = human_list
+	new_message.header.stamp = rospy.Time.now()
+	new_message.robot = robot_number_
+	new_message.fov = robot_fov
 
-	compiled_msgs_.img =  rospy.wait_for_message('/robot' + str(robot_number_) + '/camera/rgb/image_raw', Image, timeout=None)
-
-	global counter
-
-	if robot_number_ == 3:
-		#print(str(new_th_pos))
-		imgpub.publish(compiled_msgs_.img)
-		#counter = 0
-	#counter += 1
-		
-	compiled_msgs_.robot = robot_number_
-	compiled_msgs_.fov = robot_fov
-	pub.publish(compiled_msgs_)
-	compiled_msgs_.humans = []
-
-
-#Conversion Function 
-# def quaternion_to_euler_angle(x, y, z, w):
-# 	ysqr = y * y
-
-# 	t0 = +2.0 * (w * x + y * z)
-# 	t1 = +1.0 - 2.0 * (x * x + ysqr)
-# 	X = math.atan2(t0, t1)
-
-# 	t2 = +2.0 * (w * y - z * x)
-# 	t2 = +1.0 if t2 > +1.0 else t2
-# 	t2 = -1.0 if t2 < -1.0 else t2
-# 	Y = math.asin(t2)
-
-# 	t3 = 2.0 * (w * z + x * y)
-# 	t4 = 1.0 - (2.0 * (ysqr + z * z))
-# 	Z = math.atan2(t3, t4)
-	
-# 	return X, Y, Z
+	new_message.img = rospy.wait_for_message('/robot' + str(robot_number_) + '/camera/rgb/image_raw', Image, timeout=None)
+	pub.publish(new_message)
 
 def quaternion_to_euler_angle(x, y, z, w):
 	quaternion = (x, y, z, w)
 	euler = tf.transformations.euler_from_quaternion(quaternion)
-	# if robot_number_ == 3:
-	# 	print(str(euler[0]) + ' ' + str(euler[1]) + ' ' + str(euler[2]))
 	return euler[0], euler[1], euler[2]
 
 
@@ -154,52 +118,47 @@ def cartesian_to_polar_distance(x,y):
 
 #returning rad
 def cartesian_to_polar_angle(x,y):
-	return math.atan(y/x)
+	return math.atan2(y,x)
 
 def normalize_to_angle(vecX, vecY, theta):
-	vecArray = numpy.array([vecX, vecY])
-	theta *= -1.0
-	thetaCos = math.cos(theta)
-	thetaSin = math.sin(theta)
-	rotationMatrix = numpy.array([[thetaCos, -1 * thetaSin], [thetaSin, thetaCos]])
+  vecArray = numpy.matrix([[float(vecX)], [float(vecY)]])
+  theta *= -1.0
+  thetaCos = math.cos(theta)
+  thetaSin = math.sin(theta)
+  rotationMatrix = numpy.matrix([[thetaCos, -1 * thetaSin], [thetaSin, thetaCos]])
 
-	rotation = numpy.matmul(vecArray, rotationMatrix)
-	return rotation[0],rotation[1]
+  rotation = numpy.matmul(rotationMatrix, vecArray)
+  return rotation[0,0],rotation[1,0]
 
 def find(RoboPosX, RoboPosY, RoboPosTh):
+	ret = []
 	for i in range(291):
 		human_num = str(i)
+
+		# Get the coordinates of the human relative to the robot
 		hx,hy = shift_points(RoboPosX, RoboPosY, MyHumans[human_num]['x'], MyHumans[human_num]['y'])
+
+		# Get the distance between the humann and the robot
 		dist = cartesian_to_polar_distance(hx, hy)
+
 		if dist <= max_distance:  #dof
-			# hx,hy = shift_points(RoboPosX,RoboPosY, MyHumans[human_num]['x'], MyHumans[human_num]['y'])
+			# Rotoate the human and the robot so the robot is facing X=0, and get the human's new coordinates
 			relHX,relHY = normalize_to_angle(hx, hy, RoboPosTh)
+
+			# Get the new angle between the robot and the human
 			human_angle = cartesian_to_polar_angle(relHX, relHY)
-			human_angle *= -1
-			fov_offset = robot_fov / 2.0
 
-			# if robot_number_ == 3:
-			# 	print('robot ' + str(robot_angle_lower) + ', ' + str(robot_angle_upper) + ' PERSON ' + str(human_angle))
+			fov_offset = (robot_fov / 2.0) - FOV_MARGIN
+
+			# If the human has an angle less than half the robot's FOV (fov_offset)...
 			if (human_angle <= fov_offset ) and (human_angle >= (fov_offset * -1)) and (MyHumans[human_num]['dclass'] != 2):
-				# if robot_number_ == 3:
-				# 	print('Adding human')
-				if robot_number_ == 3:
-					print "robot x : ",RoboPosX
-					print "robot y : ",RoboPosY 
-					# print "human x : ",MyHumans[human_num]['x'] 
-					# print "human y : ",MyHumans[human_num]['y']
-					print 'human x : ',hx
-					print 'human y : ',hy
-					print "8====D"
-				human_msg_.id = i
-				human_msg_.dclass = MyHumans[human_num]['dclass']
-				human_msg_.angleToRobot = human_angle
-				human_msg_.distanceToRobot = dist
-				compiled_msgs_.humans.append(human_msg_)
-				break
-	# if robot_number_ == 3:
-	# 	print('---')
-
+				man = Human()
+				man.id = i
+				man.dclass = MyHumans[human_num]['dclass']
+				man.angleToRobot = human_angle
+				man.distanceToRobot = dist
+				ret.append(man)
+	return ret
 
 
 def main():
