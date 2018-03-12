929217ad1a72809a945aebdec74e0299076aa0eb
BasedLemur <logangerber1014@yahoo.com>
2018-03-11 21:06:36 -0700

    Now accurately calculates x-coordinate of where the box should be drawn for each person in the image.

--- src/multipurpose_image_draw/src/image_bounding_box_merger.cc
+++ src/multipurpose_image_draw/src/image_bounding_box_merger.cc
@@ -14,7 +14,6 @@ namespace sarwai {
     m_boxStreamPubTwo = m_nh->advertise<sensor_msgs::Image>("/robot2/camera/rgb/image_boxed", 1000);
     m_boxStreamPubThree = m_nh->advertise<sensor_msgs::Image>("robot3/camera/rgb/image_boxed", 1000);
     m_boxStreamPubFour = m_nh->advertise<sensor_msgs::Image>("robot4/camera/rgb/image_boxed", 1000);
-
   }
   
 
@@ -23,12 +22,10 @@ namespace sarwai {
   }
   
   void ImageBoundingBoxMerger::drawBoxesCallback(const detection_msgs::CompiledFakeMessageConstPtr& msg) {
-    //std::cout << "*******************************GOT IMAGE BBMERGER" << std::endl;
     // Draw boxes for each new query
     for(unsigned i = 0; i < msg->humanQueries.size(); ++i) {
       for(unsigned h = 0; h < msg->humans.size(); ++h) {
         if(msg->humans[h].id == msg->humanQueries[i]) {
-          // std::cout << "SENDING QUERY" << std::endl;
           drawBoxAndSendQuery(msg, msg->humans[h]);
           break;
         }
@@ -54,64 +51,50 @@ namespace sarwai {
   detection_msgs::BoundingBox ImageBoundingBoxMerger::drawBoxAroundHuman(sensor_msgs::Image& image, detection_msgs::Human human, float fov) const {
     detection_msgs::BoundingBox ret;
 	
-    // std::cout << "HUMAN ID: "<<human.id <<std::endl;
-    // std::cout << "IMAGE WIDTH: " << image.width << " HEIGHT: " << image.height << std::endl;
 	  unsigned yCoord = (image.height / 2) - (BOXLENGTH / 2);
     //unsigned xCoord = ((-1 * (human.angleToRobot / (fov / image.width))) + (image.width / 2)) - (BOXLENGTH / 2);
-    // Get radians per column of pixels
-    float correction_factor = 1.5;
-    //fov += correction_factor;
+    
+		// Get radians per column of pixels
     float radiansPerPixel = fov / image.width;
-    //std::cout << "RADIANS PER PIXEL: " << radiansPerPixel << std::endl;
 
     // Set the human angle in terms of radians from the left edge of the robot's FOV
       // Normally, the human is a negative angle if it's to the right of the robot's center. Multiply by -1 to get
       // the human's angle relative to the center of the robot, so that right is more positive.
-    //std::cout << "HUMAN NATURAL ANGLE: " << human.angleToRobot << std::endl;
     float humanAngle = human.angleToRobot * -1;
       // Offset the angle by half the fov, so that the human's angle is relative to the right of the robot's
       // fov (and is subsequently always positive)
-    humanAngle += fov / 2.0f;
+    float offsetHumanAngle = humanAngle + (fov / 2.0f);
 
-    // std::cout << "ANGLE FROM LEFT OF FOV: " << humanAngle << std::endl;
 
     // Get x offset of human in pixels
-    unsigned xCoord = (unsigned)(humanAngle / radiansPerPixel);
+    int xCoord = (int)(offsetHumanAngle / radiansPerPixel);
 
-    // std::cout << "HUMAN OFFSET FROM RIGHT: " << xCoord << std::endl;
 
     //Move the xCoord over by half the box's length (to get the location of the boxes top-left corner)
-    xCoord -= (BOXLENGTH / 2);
-
-    // std::cout << "BOX LEFT COORD: " << xCoord << std::endl;
-
-    // std::cout << "FOV: " << fov << " ANGLE: " << human.angleToRobot << std::endl;
-    //std::cout << "X: " << xCoord << " Y: " << yCoord << std::endl;
+    int movedXCoord = xCoord - (BOXLENGTH / 2);
 
     cv_bridge::CvImagePtr cvImage;
     cvImage = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::BGR8);
     cv::Mat imageMatrix = cvImage->image;
-    cv::Point topLeftCorner = cv::Point(xCoord, yCoord);
-    cv::Point bottomRightCorner = cv::Point(xCoord + BOXLENGTH, yCoord + BOXLENGTH);
+    cv::Point topLeftCorner = cv::Point(movedXCoord, yCoord);
+    cv::Point bottomRightCorner = cv::Point(movedXCoord + BOXLENGTH, yCoord + BOXLENGTH);
     cv::rectangle(imageMatrix, topLeftCorner, bottomRightCorner, 50);
     image = *(cv_bridge::CvImage(image.header, "bgr8", imageMatrix).toImageMsg());
 
-    ret.xmin = xCoord;
+    ret.xmin = movedXCoord;
     ret.ymin = yCoord;
     ret.xmax = bottomRightCorner.x;
     ret.ymax = bottomRightCorner.y;
-    // std::cout << "=====" << std::endl;
     return ret;
   }
 
   void ImageBoundingBoxMerger::sendBoxedStream(const detection_msgs::CompiledFakeMessageConstPtr& msg) const {
     sensor_msgs::Image imageCopy(msg->img);
+    unsigned id = msg->robot;
     for(unsigned i = 0; i < msg->humans.size(); ++i) {
-      // std::cout << "Drawing box" << std::endl;
       drawBoxAroundHuman(imageCopy, msg->humans[i], msg->fov);
     }
     
-    unsigned id = msg->robot;
     if(id == 1) {
       m_boxStreamPubOne.publish(imageCopy);
     }
@@ -119,7 +102,6 @@ namespace sarwai {
       m_boxStreamPubTwo.publish(imageCopy);
     }
     else if(id == 3) {
-      // std::cout << "Publishing to stream" << std::endl;
       m_boxStreamPubThree.publish(imageCopy);
     }
     else {
