7d0ffb329ddd51e9e1e8322862fe61c3c463f1a0
Kenny Hanson <hennythehen@gmail.com>
2018-03-21 22:46:57 -0700

    added rtree wall wrapper

---
+++ src/detection_calculation/src/detection_calculation_node/wall_rtree.py
@@ -0,0 +1,43 @@
+import rtree
+import yaml
+
+
+
+class WallRtree:
+    def search(self, left, bottom, right, top):
+        return self.spatial_walls.intersection(((left, bottom, right, top)))
+
+    def __init__(self, wall_file):
+        walls_yaml = yaml.load(open(wall_file))
+
+        self.spatial_walls = rtree.index.Index()
+        for wall_id, seg_list in walls_yaml.iteritems():
+            # tuples. [0] = x, [1] = y
+            tl = seg_list['p3']
+            br = seg_list['p2']
+            yaw = seg_list['yaw']
+
+            left = tl[0]
+            top = tl[1]
+            right = br[0]
+            bottom = br[1]
+
+            try:
+                if yaw == 0:
+                    self.spatial_walls.add(wall_id, (left, bottom, right, top))
+
+                if yaw == 1.5708:
+                    self.spatial_walls.add(wall_id, (top, left, bottom, right))
+
+                if yaw == -1.5708:
+                    self.spatial_walls.add(wall_id, (bottom, right, top, left))
+
+                if yaw == 3.14159:
+                    self.spatial_walls.add(wall_id, (right, top, left, bottom))
+
+                # print left, bottom, right, top
+                # suc_count += 1
+            except:
+                # self.spatial_walls.add(wall_id, ())
+                print "failed on: {}".format(wall_id)
+                exit(-1)
