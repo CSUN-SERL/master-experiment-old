e333baefe6f0ac5b560e78fc3bcba81292fca8ab
BasedLemur <logangerber1014@yahoo.com>
2018-03-27 19:03:34 -0700

    Added forced and missed detection functionality

--- src/detection_logger/src/visual_logger_node.cc
+++ src/detection_logger/src/visual_logger_node.cc
@@ -5,6 +5,8 @@
 #include <cv_bridge/cv_bridge.h>
 
 #include "detection_msgs/ProcessedVisualDetection.h"
+#include "detection_msgs/HumanSeen.h"
+
 #include "visual_detection_data.h"
 #include "visual_logger.h"
 #include "socketio_visual_logger.h"
@@ -13,6 +15,7 @@
 using namespace sarwai;
 
 void LogVisualDetection(const detection_msgs::ProcessedVisualDetection::ConstPtr&);
+void SendHumanViewed(const detection_msgs::HumanSeen::ConstPtr& msg);
 
 VisualLogger visual_logger(".");
 SocketIOVisualLogger socketio_logger(".", "http://192.168.1.11", 8000);
@@ -27,7 +30,9 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "visual_logger");
   ros::NodeHandle nh;
   std::string visual_detection_topic = "/sarwai_detection/detection_processeddetection";
+  std::string human_view_topic = "/human_camera_seen";
   ros::Subscriber visual_sub = nh.subscribe(visual_detection_topic.c_str(), 1000, LogVisualDetection);
+  ros::Subscriber human_in_view_sub = nh.subscribe(human_view_topic.c_str(), 1000, SendHumanViewed);
 
   ros::spin();
 }
@@ -41,14 +46,22 @@ void LogVisualDetection(const detection_msgs::ProcessedVisualDetection::ConstPtr
   data.left_x_coord = msg->bounding_box.xmin;
   data.top_y_coord = msg->bounding_box.ymin;
   data.timestamp = (int) msg->image.header.stamp.sec;
-  data.confidence_rating = msg->bounding_box.probability;
-  //data.object_class = msg->bounding_box.Class;
   data.object_class = "Human";
   data.robot_id = msg->robotId;
+  data.human_id = msg->human_id;
 
   // Convert ROS image to OpenCV image
   cv::Mat cvimage = cv_bridge::toCvCopy(msg->image, sensor_msgs::image_encodings::BGR8)->image;
 
   std::string image_filename = visual_logger.Log(cvimage, data);
-  socketio_logger.Log(image_filename, data);
+  if(msg->forced) {
+    socketio_logger.SendHuman(msg->human_id, msg->robotId, true);
+  }
+  else {
+    socketio_logger.Log(image_filename, data);
+  }
+}
+
+void SendHumanViewed(const detection_msgs::HumanSeen::ConstPtr& msg) {
+  socketio_logger.SendHuman(msg->human_id, msg->robot_id, false);
 }
